796c18677a5ec606a77808c75ae2ac64bb44f59d
stonega <xijieyin@gmail.com>
2024-08-21 16:13:10 +0800

    feat: add max btc transaction

--- src/index.ts
+++ src/index.ts
@@ -493,6 +493,158 @@ export async function createSendMultiBTC({
   return psbt;
 }
 
+export async function createSendMaxBTC({
+  utxos,
+  receivers,
+  wallet,
+  network,
+  changeAddress,
+  feeRate,
+  dump,
+  data,
+}: {
+  utxos: UnspentOutput[];
+  receivers: {
+    address: string;
+    amount?: number;
+  }[];
+  wallet: any;
+  network: any;
+  changeAddress: string;
+  feeRate?: number;
+  dump?: boolean;
+  data?: string;
+}) {
+  const tx = new OrdTransaction(wallet, network, feeRate);
+  await tx.initBitcoin();
+  tx.setChangeAddress(changeAddress);
+  const to = receivers.find((v) => v.amount === undefined).address;
+  if(!to) {
+    throw new Error("No receiver found, please provide one receiver with undefined amount");
+  }
+  const nonOrdUtxos: UnspentOutput[] = [];
+  const ordUtxos: UnspentOutput[] = [];
+  utxos.forEach((v) => {
+    if (v.ords.length > 0) {
+      ordUtxos.push(v);
+    } else {
+      nonOrdUtxos.push(v);
+    }
+  });
+
+  receivers.forEach((v) => {
+    if(v.amount) {
+      tx.addOutput(v.address, v.amount);
+    } else {
+      tx.addOutput(v.address, 0);
+    }
+  });
+
+  if (data) tx.addOpReturnOutput(data);
+
+  const outputAmount = tx.getTotalOutput();
+
+  let tmpSum = tx.getTotalInput();
+  if (nonOrdUtxos.length === 0) {
+    throw new Error("Balance not enough");
+  }
+  for (let i = 0; i < nonOrdUtxos.length; i++) {
+      const nonOrdUtxo = nonOrdUtxos[i];
+      tx.addInput(nonOrdUtxo);
+  }
+  const fee = await tx.calNetworkFee();
+  const left = tmpSum - outputAmount - fee;
+  if(left < UTXO_DUST) {
+    throw new Error(
+      `Balance not enough. Need ${satoshisToAmount(
+        outputAmount + fee
+      )} BTC as network fee, but only ${satoshisToAmount(tmpSum)} BTC.`
+    );      
+  }
+
+   tx.outputs.find(o => o.value === 0)!.value  = left;
+
+  const unspent = tx.getUnspent();
+  if (unspent === 0) {
+    throw new Error("Balance not enough to pay network fee.");
+  }
+
+  const psbt = await tx.createSignedPsbt();
+  if (dump) {
+    tx.dumpTx(psbt);
+  }
+
+  return psbt;
+}
+
+export async function calculateMaxBtc({
+  utxos,
+  receivers,
+  network,
+  changeAddress,
+  feeRate,
+  data,
+}: {
+  utxos: UnspentOutput[];
+  receivers: {
+    address: string;
+    amount?: number;
+  }[];
+  network: any;
+  changeAddress: string;
+  feeRate?: number;
+  data?: string;
+}) {
+  const tx = new OrdTransaction(undefined, network, feeRate);
+  await tx.initBitcoin();
+  tx.setChangeAddress(changeAddress);
+  const to = receivers.find((v) => v.amount === undefined).address;
+  if(!to) {
+    throw new Error("No receiver found, please provide one receiver with undefined amount");
+  }
+  const nonOrdUtxos: UnspentOutput[] = [];
+  const ordUtxos: UnspentOutput[] = [];
+  utxos.forEach((v) => {
+    if (v.ords.length > 0) {
+      ordUtxos.push(v);
+    } else {
+      nonOrdUtxos.push(v);
+    }
+  });
+
+  receivers.forEach((v) => {
+    if(v.amount) {
+      tx.addOutput(v.address, v.amount);
+    } else {
+      tx.addOutput(v.address, 0);
+    }
+  });
+
+  if (data) tx.addOpReturnOutput(data);
+
+  const outputAmount = tx.getTotalOutput();
+
+  let tmpSum = tx.getTotalInput();
+  if (nonOrdUtxos.length === 0) {
+    throw new Error("Balance not enough");
+  }
+  for (let i = 0; i < nonOrdUtxos.length; i++) {
+      const nonOrdUtxo = nonOrdUtxos[i];
+      tx.addInput(nonOrdUtxo);
+  }
+  const fee = await tx.calNetworkFee();
+  const left = tmpSum - outputAmount - fee;
+  if(left < UTXO_DUST) {
+    throw new Error(
+      `Balance not enough. Need ${satoshisToAmount(
+        outputAmount + fee
+      )} BTC as network fee, but only ${satoshisToAmount(tmpSum)} BTC.`
+    );      
+  }
+
+  return left
+}
+
 export async function createSendRunes({
   utxos,
   receivers,
